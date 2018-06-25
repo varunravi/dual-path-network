4de39050da5aef7cf058b03f738c0837891e801a
varunravi <root@Varuns-MacBook-Pro.local>
2018-06-25 16:38:11 -0700

    dpn implemented. need to test data

--- dpn.py
+++ dpn.py
@@ -9,27 +9,43 @@ import os
 import tensorflow as tf
 
 
+def res_net(layer, filter_size, padding='same'):
 
-def micro_block(layer, final_filter_size, num_iterations, activation=tf.nn.relu):
+	layer = tf.layers.conv2d(layer, filter_size, (3,3),activation=tf.nn.relu, padding=padding)
+	layer = tf.layers.conv2d(layer, filter_size,(3,3), padding=padding)
 
-	layer_dense_path = tf.placeholder(tf.float32)
-	layer_res_path = tf.placeholder(tf.float32)
+	return layer
 
-	layer_dense_path = layer
-	layer_res_path = layer
+# def dense_net():
+
+
+
+def micro_block(layer, final_filter_size, num_iterations, cardinality=32, activation=None, padding = 'same'):
+
+	for i in range (0, num_iterations):
+		initial_filter_size = final_filter_size / (2+2/3)
+
+		layer = tf.layers.conv2d(layer, initial_filter_size, (1,1), activation=activation, padding=padding)
+		
+		layer_G = []
+		for i in range(0, 32):
+			layer_G.append(tf.layers.conv2d(layer, initial_filter_size, (3,3), activation=activation, padding=padding))
+		
+		layer = tf.concat(values=layer_G, axis=3, name='concat')
+		layer = tf.layers.conv2d(layer, 256, (1,1), activation=activation, padding=padding)
+
+
+
+	return layer
+
+def macro_block(dense_net, layer, filter_size, micro_iter):
 	
-	
-	for i in range(0, num_iterations):
-		layer = tf.layers.conv2d(layer, final_filter_size/(2+2/3), (1,1), activation=activation)
-		layer = tf.layers.conv2d(layer, final_filter_size/(2+2/3), (3,3), activation=activation)
-		layer = tf.layers.conv2d(layer, final_filter_size, (1,1), activation=activation)
+	layer = micro_block(layer, filter_size, micro_iter)
+	res_layer = res_net(layer, filter_size)
+	dense_layer = tf.concat([dense_net, layer], axis=3)
+	layer = tf.concat([dense_layer, res_layer], axis=3)
 
-	layer_dense_path = tf.image.resize_nearest_neighbor(layer_dense_path, (layer.get_shape().as_list()[1],layer.get_shape().as_list()[2]))
-	layer_res_path = tf.image.resize_nearest_neighbor(layer_res_path, (layer.get_shape().as_list()[1],layer.get_shape().as_list()[2]))
-	
-
-	return tf.concat([layer, layer_dense_path, layer_res_path], axis=-1)
-
+	return [layer,dense_layer]
 
 def dpn(X_train, y_train, activation=tf.nn.relu):
 
@@ -37,25 +53,40 @@ def dpn(X_train, y_train, activation=tf.nn.relu):
 	y = tf.placeholder(tf.float32, shape=[y_train.shape[0], y_train.shape[1], y_train.shape[2], y_train.shape[3]], name='y')
 
 	# conv1
-	conv1 = tf.layers.conv2d(x, 64, (7,7), activation=activation)
+	conv1 = tf.layers.conv2d(x, 64, (7,7), (2,2), activation=activation, padding='same')
+	conv2 = tf.layers.max_pooling2d(inputs=conv1, pool_size=(3,3), strides=2, padding='same')
 	
 	# conv2
-	conv2 = tf.layers.conv2d(conv1, 64, (3,3), activation=activation)
-	conv2 = tf.layers.max_pooling2d(inputs=conv1, pool_size=(3,3), strides=2)
-	
 	conv2 = micro_block(conv2, 256, 3)
-	conv3 = micro_block(conv2, 512, 4)
-	conv4 = micro_block(conv3, 1024, 20)
-	conv5 = micro_block(conv4, 2048, 3)
+	res_conv2 = res_net(conv2, 256)
+	dense_net = conv2
+	conv2 = tf.concat([res_conv2, conv2], axis=3)
 
-	final = tf.layers.conv2d(conv5, 2, 1, activation=tf.nn.softmax)
+	layer, dense_net = macro_block(dense_net, conv2, 512, 4)
+	layer, dense_net = macro_block(dense_net, layer, 1024, 20)
+	layer, _ = macro_block(dense_net, layer, 2048, 3)
+	
+	layer = tf.layers.average_pooling2d(layer, (2,2), 2)
+	layer = tf.contrib.layers.fully_connected(layer, 1000)
 
-	ipdb.set_trace()
+	final = tf.layers.conv2d(layer, 2, 1, activation=tf.nn.softmax)
 
+	return final
 
 if __name__ == '__main__':
 
 	X_image = np.zeros((1, 572, 572, 1))
 	y_image = np.zeros((1, 572, 572, 1))
 
-	dpn(X_image, y_image)
+	dpn_model = dpn(X_image, y_image)
+
+	mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+	train_data = mnist.train.images # Returns np.array
+	train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+	eval_data = mnist.test.images # Returns np.array
+	eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+	ipdb.set_trace()
+
+	
+
